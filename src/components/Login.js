72eefedd278e82a45703ad39850e8d26838a1e50
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-01-29 13:50:59 +0800

    Improve UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import { Button, Form } from "react-bootstrap"
 import { useState } from "react"
 import { useNavigate } from "react-router"
 import axios from "axios"
+import "../styles/Login.scss"
 
 export default function Login(props) {
     const [signupAccount, setSignupAccount] = useState("")
@@ -20,24 +21,25 @@ export default function Login(props) {
     
 
     return (
-    <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>帳號</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" value={signupAccount} onChange={(e) => {setSignupAccount(e.target.value)}}/>
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>密碼</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={signupPassword} onChange={(e) => {setSignupPassword(e.target.value)}}/>
-        </Form.Group>
-        <Button variant="primary" onClick={signUp}>
-            登入
-        </Button>
-        <Form.Text className="text-muted">
-            還沒有帳號嗎？<Button onClick={() => {props.fun(true)}}>註冊</Button>
-        </Form.Text>
-    </Form>    
+        <div className="login">
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>帳號</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" value={signupAccount} onChange={(e) => {setSignupAccount(e.target.value)}}/>
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>密碼</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={signupPassword} onChange={(e) => {setSignupPassword(e.target.value)}}/>
+                </Form.Group>
+                <Button variant="primary" onClick={signUp}>
+                    登入
+                </Button>
+
+                <Button onClick={() => {props.fun(true)}}>註冊</Button>
+            </Form>
+        </div>
     )
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
 import { Button, Form } from "react-bootstrap"
 import { useState } from "react"
-//import "../styles/Signup.scss"
+import "../styles/Login.scss"
 import { useNavigate } from "react-router"
 import axios from "axios"
 
@@ -29,35 +29,33 @@ export default function Signup(props) {
     
 
     return (
-    <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>帳號</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" value={signupAccount} onChange={(e) => {setSignupAccount(e.target.value)}}/>
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicNickname">
-            <Form.Label>暱稱</Form.Label>
-            <Form.Control placeholder="Enter Nickname" value={signupNickname} onChange={(e) => {setSignupNickname(e.target.value)}}/>
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>密碼</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={signupPassword} onChange={(e) => {setSignupPassword(e.target.value)}}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>確認密碼</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={signupPasswordCheck} onChange={(e) => {setSignupPasswordCheck(e.target.value)}}/>
-        </Form.Group>
-        <Button variant="primary" onClick={signUp}>
-            註冊
-        </Button>
-        <Form.Text className="text-muted">
-            已經有帳號？<Button onClick={() => {props.fun(false)}}>登入</Button>
-        </Form.Text>
-    </Form>    
+        <div className="login">
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>帳號</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" value={signupAccount} onChange={(e) => {setSignupAccount(e.target.value)}}/>
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicNickname">
+                    <Form.Label>暱稱</Form.Label>
+                    <Form.Control placeholder="Enter Nickname" value={signupNickname} onChange={(e) => {setSignupNickname(e.target.value)}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>密碼</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={signupPassword} onChange={(e) => {setSignupPassword(e.target.value)}}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>確認密碼</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={signupPasswordCheck} onChange={(e) => {setSignupPasswordCheck(e.target.value)}}/>
+                </Form.Group>
+                <Button variant="primary" onClick={signUp}>
+                    註冊
+                </Button>
+                <Button onClick={() => {props.fun(false)}}>登入</Button>
+                
+            </Form>
+        </div>
     )
 }
