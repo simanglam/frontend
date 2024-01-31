e1d15f9c06979937d57e6a2b4a2809ad59317b89
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-01-31 13:37:07 +0800

    Add Message component and Message.scss file

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap"
 
+import "../styles/Message.scss"
 
 export default function Messages(props){
     return(
-        <>
+        <div className="messages">
             {props.messages.map((message) => {
                 return(
-                    <h1 key={message.id}>{message.text} - {message.author}</h1>
+                    <div id="message" key={message.id}>
+                        <Container>
+                            <Row>
+                                <Col id="left-col">{message.author}</Col>
+                                <Col id="right-col">{message.created}</Col>
+                            </Row>
+                            <Row>
+                                <Col>{message.text}</Col>
+                            </Row>
+                        </Container>
+                    </div>
                 )
             })}
-        </>
+        </div>
     )
 }
